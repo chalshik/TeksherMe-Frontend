ddd5a047cfe7e6b8a0eae88e4584e008a1803904
BekaScript <bekbolsunysmanov07@gmail.com>
2025-04-25 14:51:39 +0600

    a

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'explore_screen.dart';
+import 'bookmarks_screen.dart';
+import 'settings_screen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  
+  static const List<Widget> _screens = [
+    HomeScreen(),
+    ExploreScreen(),
+    BookmarksScreen(),
+    SettingsScreen(),
+  ];
+  
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            label: 'Bookmarks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+} 
